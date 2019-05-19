f90f6dc363eafd5ffcf4af193185a520d50a7ce0
joulert02 <50017852+joulert02@users.noreply.github.com>
2019-05-18 21:17:06 -0500

    migracioness

--- database/migrations/2019_05_17_204613_crear_tabla_permiso_rol.php
+++ database/migrations/2019_05_17_204613_crear_tabla_permiso_rol.php
@@ -16,9 +16,9 @@ class CrearTablaPermisoRol extends Migration
         Schema::create('permiso_rol', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('rol_id');
-            $table->foreign('rol_id', 'fk_permisorol_rol')->references('id')->on('rol')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('rol_id', 'fk_permisorol_rol')->references('id')->on('rol');
             $table->unsignedInteger('permiso_id');
-            $table->foreign('permiso_id', 'fk_permisorol_permiso')->references('id')->on('permiso')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('permiso_id', 'fk_permisorol_permiso')->references('id')->on('permiso');
             $table->timestamps();
         });
     }
